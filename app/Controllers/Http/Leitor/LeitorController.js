c4d55bad5f9c548c2311e60a7bf94335d945aed4
JoaquimDonga <63249720+JoaquimDonga@users.noreply.github.com>
2020-08-28 20:06:49 +0100

    Sucesso

--- app/Controllers/Http/Leitor/LeitorController.js
+++ app/Controllers/Http/Leitor/LeitorController.js
@@ -17,6 +17,7 @@ class LeitorController {
        let dados = [pkLeitor,nome,bi,endereco,telefone1,telefone2,email,password] //Criando o array com os dados
        let result = await leitorModel.Adicionar(dados) //inserindo os dados
        let formatoJson  = toJson.Create(result)
+       formatoJson.data = [pkLeitor,nome,bi,endereco,telefone1,telefone2,email]
         response.json(formatoJson)
      }
 
